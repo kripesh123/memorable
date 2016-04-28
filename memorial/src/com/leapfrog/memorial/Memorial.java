351f4acc11f2a5f7dba77ab3f20088ad5326c88d
Kripesh Bista <kripeshbista3@gmail.com>
2016-04-28 14:10:31 +0545

    For loop Added

--- memorial/src/com/leapfrog/memorial/Memorial.java
+++ memorial/src/com/leapfrog/memorial/Memorial.java
@@ -15,7 +15,9 @@ public class Memorial {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        for(int i=1;i<=10;i++){
+            System.out.println(i);
+        }
     }
     
 }
